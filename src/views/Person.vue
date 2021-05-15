0b0acf80b3afb722ce27595da248954d4704a836
unknown <78619347+Jovany-Vergara@users.noreply.github.com>
2021-05-14 21:02:06 -0500

    Agregado por vue

---
+++ src/views/Person.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="about">
+    <h1>{{message}}</h1>
+  <button v-on:click="imprimir">Guardar</button>
+  <p>Se ha hecho clic en el botón de arriba {{ counter }} veces</p>
+  <div class="row">
+      <div class="col">
+          Name:
+      </div>
+      <div class="col">
+          <input type="text" v-model="person.name">
+      </div>
+<p></p>
+      <div class="col">
+          First Name:
+      </div>
+      <div class="col">
+          <input type="text" v-model= "person.apellidos">
+      </div>
+      <p></p>
+
+    <div class="col">
+        Age:
+    </div>
+    <div class="col">
+        <input type="text" v-model= "person.edad">
+        
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+      <div class="col">
+          Lista de personas
+          <li>
+              Persona Agregada: {{listPersonas }}
+          </li>
+      </div>
+  </div>
+  <hr>
+  <div class="row">
+      <div class="col">
+          Persona Encontrada
+          Indica Nombre: 
+          <input type="text" v-model="nameToSearch">
+          <button v-on:click="buscar">Buscar</button>
+          <li>Encontrada: {{personFound}} </li>
+      </div>
+  </div>
+  
+  </div>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Person',
+  data: function() {
+    return {
+        person: {
+            name: "",
+            apellidos: "",
+            edad: null,
+            
+        },
+        counter: 0,
+        message: "",
+        listPersonas: [],
+        nameToSearch: "",
+        personFound: {}
+    } 
+  },
+  methods: {
+    imprimir: function(){
+        console.log(this.person)
+        this.listPersonas.push({...this.person})
+    },
+    buscar: function(){
+        this.personFound =  this.listPersonas.find(personQueItera => personQueItera.name == this.nameToSearch)
+    }
+
+  },
+  components: {
+    
+  }
+}
+</script>
