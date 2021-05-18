cb1ff5fa1242aa1847bab10626e5c57cfc0aeb41
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-05-18 18:36:33 -0500

    el index de prueba

---
+++ src/views/Prueba.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="prueba">
+    <h1>Aplicación De La Familia ❤️ </h1>
+    <main class="container d-flex align-items-center justify-content-center ">
+        <div class="row">
+          <!-- Columna Izquierda -->
+          <div class="col-12 col-lg-9 ">
+            <div class="row">
+              <div class="col-12 col-lg-8 p-2">
+                <article class="card h-100 bg-primary bg-gradient text-white">
+                  <div class="card-body">
+                    <div class="d-flex mb-3">
+                      <img alt="" src="../assets/bran.jpg" class="border border-white border-3 rounded-circle image-size">
+                      <div class="ps-lg-3">
+                        <h6 class="m-0 h6">Vergara Brandon</h6>
+                        <p class="m-0 h6">Developer From Software </p>
+                      </div>
+                    </div>
+                    <div>
+                      <p class="h6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo perspiciatis suscipit eius, adipisci ipsum eaque natus,
+                         odit rem voluptatum expedita quae animi minima! Ipsa, repellendus sequi. Omnis fuga perspiciatis possimus!</p>
+                      <p class="fs-6 lh-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo perspiciatis suscipit eius, adipisci ipsum eaque natus,
+                         odit rem voluptatum expedita quae animi minima! Ipsa, repellendus sequi. Omnis fuga perspiciatis possimus!</p>
+                    </div>
+                  </div>
+                </article>
+              </div>
+
+              <div class="col-12 col-lg-4 p-2">
+                <article class="card h-100 bg-warning bg-gradient ">
+                  <div class="card-body">
+                    <div class="d-flex mb-3">
+                      <img alt="" src="../assets/Fami/ray.jpeg" class="border border-dark border-3 rounded-circle image-size">
+                      <div class="ps-lg-3">
+                        <h6 class="m-0 h6">Vergara Ray</h6>
+                        <p class="m-0 h6">Car Driver </p>
+                      </div>
+                    </div>
+                    <div>
+                      <p class="h6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo perspiciatis suscipit eius, adipisci ipsum eaque natus,
+                         odit rem voluptatum expedita quae animi minima! Ipsa, repellendus sequi. Omnis fuga perspiciatis possimus!</p>
+                      
+                    </div>
+                  </div>
+                </article>
+              </div>
+
+              <div class="col-12 col-lg-4 p-2">
+                <article class="card h-100 bg-danger bg-gradient text-white">
+                  <div class="card-body">
+                    <div class="d-flex mb-3">
+                      <img alt="" src="../assets/Fami/karla.jpeg" class="border border-primary border-3 rounded-circle image-size">
+                      <div class="ps-lg-3">
+                        <h6 class="m-0 h6">Galina Karla</h6>
+                        <p class="m-0 h6">Psicologa </p>
+                      </div>
+                    </div>
+                    <div>
+                      
+                      <p class="fs-6 lh-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo perspiciatis suscipit eius, adipisci ipsum eaque natus,
+                         odit rem voluptatum expedita quae animi minima! Ipsa, repellendus sequi. Omnis fuga perspiciatis possimus!</p>
+                    </div>
+                  </div>
+                </article>
+
+              </div>
+              <div class="col-12 col-lg-8 p-2">
+                <article class="card h-100 bg-warning bg-dark bg-gradient text-white">
+                  <div class="card-body">
+                    <div class="d-flex mb-3">
+                      <img alt="" src="../assets/Fami/yo.jpeg" class="border border-primary border-3 rounded-circle image-size">
+                      <div class="ps-lg-3">
+                        <h6 class="m-0 h6">Vergara Jovany</h6>
+                        <p class="m-0 h6">Developer From Software </p>
+                      </div>
+                    </div>
+                    <div>
+                      <p class="h6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo perspiciatis suscipit eius, adipisci ipsum eaque natus,
+                         odit rem voluptatum expedita quae animi minima! Ipsa, repellendus sequi. Omnis fuga perspiciatis possimus!</p>
+                      <p class="fs-6 lh-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo perspiciatis suscipit eius, adipisci ipsum eaque natus,
+                         odit rem voluptatum expedita quae animi minima! Ipsa, repellendus sequi. Omnis fuga perspiciatis possimus!</p>
+                    </div>
+                  </div>
+                </article>
+
+              </div>
+            </div>
+          </div>
+          <!-- Columna Derecha -->
+          <div class="col-12 col-lg-3 p-2">
+            <article class="card h-100 bg-info">
+                  <div class="card-body">
+                    <div class="d-flex mb-3">
+                      <img alt="" src="../assets/Fami/olga.jpeg" class="border border-danger border-3 rounded-circle image-size">
+                      <div class="ps-lg-3">
+                        <h6 class="m-0 h6">Magaña Olga</h6>
+                        <p class="m-0 h6">Housewife </p>
+                      </div>
+                    </div>
+                    <div>
+                      <p class="h6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo perspiciatis suscipit eius, adipisci ipsum eaque natus,
+                         odit rem voluptatum expedita quae animi minima! Ipsa, repellendus sequi. Omnis fuga perspiciatis possimus!</p>
+                      <p class="fs-6 lh-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo perspiciatis suscipit eius, adipisci ipsum eaque natus,
+                         odit rem voluptatum expedita quae animi minima! Ipsa, repellendus sequi. Omnis fuga perspiciatis possimus!</p>
+                    </div>
+                  </div>
+                </article>
+
+          </div>
+        </div>
+    </main>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script> 
+
+<style lang="scss" scoped>
+.image-size {
+  width: 20%;
+}
+@media (min-width: 992px) {
+  .alto-100 {
+    height: 100vh;
+  }
+}
+
+</style>
+<style>
+
+</style>
